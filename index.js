f6d1137d106c95f1ee4e5b3733dd899f84e3537f
benjaminnoble <benjaminanoble@icloud.com>
2021-07-19 22:16:04 +1000

    Done with assignment

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// Write your solution in this file!
+const driver = {
+    name: 'Sam',
+    addres: '11 Broadway'
+}
+
+function updateDriverWithKeyAndValue(driver, key, value) {
+    return Object.assign({}, driver, { [key] : value });
+}
+
+function destructivelyUpdateDriverWithKeyAndValue(driver, key, value) {
+    driver[key] = value;
+
+    return driver;
+  }
+
+  function deleteFromDriverByKey(driver, key) {
+      const driverTwo = Object.assign({}, driver);
+      delete driverTwo.name;
+      return driverTwo; 
+    }
+
+    function destructivelyDeleteFromDriverByKey(driver, key) {
+        delete driver [key];
+        return driver;
+    }
